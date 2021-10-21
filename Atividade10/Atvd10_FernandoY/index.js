fa2e1da2b5045b7e6c29b2941a1b4f1a18f686b2
Fernando Yuji Mochizuki Okano <69857686+FernandoY2203@users.noreply.github.com>
2021-10-21 16:35:46 -0300

    Add files via upload

---
+++ Atividade10/Atvd10_FernandoY/index.js
@@ -0,0 +1,16 @@
+
+//Metodo 1
+var Aluno1 = new Object();
+Aluno1.ra = "0030482011010"
+Aluno1.nome = "Fernando Yuji"
+
+//Metodo 2
+var Aluno1 = {};
+Aluno1.ra = "0030482011010"
+Aluno1.nome = "Fernando Yuji"
+
+//Metodo 3
+var Aluno1 = {
+    ra: "0030482011010",
+    nome: "Fernando Yuji"
+}
